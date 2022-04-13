cee417f6307614d22972d2b08e4ba9c365a838b1
jmtheo8 <98934584+jmtheo8@users.noreply.github.com>
2022-04-13 04:53:19 +0000

    Completed prework 05

---
+++ pre04/Beer.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Beer {
+    public static void main(String[] args){
+      
+      int n; 
+      n = 99;
+  
+        if (n > 0) {
+        System.out.println((n - 1) + " " + "Bottles of beer on the wall");
+ } else {
+     System.out.println("No bottles of beer on the wall, no bottles of beer, ya’ can’t take one down, ya’ can’t pass it around, ’cause there are no more bottles of beer on the wall!");
+ }
+}
+}
