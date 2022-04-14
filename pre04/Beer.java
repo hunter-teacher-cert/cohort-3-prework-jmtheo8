a9060004c4733b2c6d39db1d6c0967a27dc3feeb
jmtheo8 <98934584+jmtheo8@users.noreply.github.com>
2022-04-14 16:08:49 +0000

    Completed Prework Assignment 04

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -4,13 +4,16 @@ import java.util.*;
 public class Beer {
     public static void main(String[] args){
       
-      int n; 
-      n = 99;
+      int b = 100; 
+      int sub = 1;
   
-        if (n > 0) {
-        System.out.println((n - 1) + " " + "Bottles of beer on the wall");
- } else {
+      while (b > 0) {
+        System.out.println( b + " " + "Bottles of beer on the wall." + " " + b + " " + "Bottles of beer." + " " + "Take one down pass it around." + " " + (b - 1) + " " + "Bottles of beer on the wall.");
+         
+          sub = b - sub;
+            b = b - 1;
+ } 
+        
      System.out.println("No bottles of beer on the wall, no bottles of beer, ya’ can’t take one down, ya’ can’t pass it around, ’cause there are no more bottles of beer on the wall!");
  }
-}
 }
