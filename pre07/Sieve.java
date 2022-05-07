6438bab3685d9e64c2461dbf9481b14d6e1d6adb
jmtheo8 <98934584+jmtheo8@users.noreply.github.com>
2022-05-07 02:19:09 +0000

    Completed Sieve.java. Also note that prework 07's Array and Max assignments were completed , committed and pushed 10 days ago. Thanks.

--- pre07/Sieve.java
+++ pre07/Sieve.java
@@ -1,19 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-public class Sieve {
-public static void main(String[] args){
+public class Sieve{
+public static void sieve(int n){
+    
+  boolean primeNum[] = new boolean[n+1];
+  
+    for(int i=0;i<=n;i++)        
+      primeNum[i] = true;
+         
+        for(int x = 2; x*x <=n; x++){
+          if(primeNum[x] == true){
+             
+           for(int i = x*x; i <= n; i += x)
+             primeNum[i] = false;
+           }
+          }
 
-  int i = 1, notPrime = 0;
-  while(i <= n){
-  if(n % i == 0){
-  notPrime++;
+        for(int i = 2; i <= n; i++){
+         if(primeNum[i] == true)
+          System.out.print(i + " ");
+         }
+        }
+
+public static void main(String args[]){
+  int n = 74;
+    if (true){
+
+  System.out.println("This are all the prime numbers that ");
+
+  System.out.println("are smaller or equal to the number " + n + ":");
+        
+    Sieve m = new Sieve();
+    m.sieve(n);
   }
-  i++;
-  }
-  if (notPrime == 2){
-  return 1;
-  }
-   return 0;
-  }
+ }
 }
